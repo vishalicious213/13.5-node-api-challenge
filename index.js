9a6f534f52e13acd1a3ecd1f184e6411e9cb1164
Vish Singh <vishalicious213@gmail.com>
2020-06-12 17:20:08 -0700

    installed express & added basic server code to index.js

--- index.js
+++ index.js
@@ -12,3 +12,15 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Go code!
 */
+
+const express = require('express');
+
+const server = express();
+
+server.get('/', (req, res) => {
+    res.send('Hello from Express');
+});
+
+server.listen(5000, () =>
+    console.log('Server running on http://localhost:5000')
+);
