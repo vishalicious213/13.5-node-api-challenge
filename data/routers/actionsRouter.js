90f280410a95b55f39150f094ac7255a1fb7d038
Vish Singh <vishalicious213@gmail.com>
2020-08-14 18:11:16 -0700

    GET array of actions returns data - project_id check not working

--- data/routers/actionsRouter.js
+++ data/routers/actionsRouter.js
@@ -4,10 +4,23 @@ const router = express.Router();
 // import helpers from actionModel
 const actionUtils = require('../helpers/actionModel');
 
-// GET array of actions
+// GET array of actions - project_id check not working
 router.get('/', (req, res) => {
     const project_id = Number(req.params.project_id);
+    console.log('proj_id: ', project_id);
+    console.log('req proj_id: ', req.params.project_id);
+    console.log('req params: ', req.params);
     actionUtils.get(project_id)
+        // .then(action => {
+        //     if (action.project_id) {
+        //         res.status(200).json(action)
+        //     } else {
+        //         res.status(404).json({
+        //             message: 'The action does not exist',
+        //         })
+        //     }
+        // })
+
         .then(action => res.status(200).json(action))
         .catch(error => res.status(500).json({message: "Error retrieving actions"}))
 })
